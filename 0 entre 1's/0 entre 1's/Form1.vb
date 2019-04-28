3c2a8f58d15591ee1476f9c154b053478c35b34e
Jarego <jaregocreepy@hotmail.com>
2019-04-28 01:58:34 -0500

    Add files via upload

---
+++ 0 entre 1's/0 entre 1's/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Dim contador = 0
+    Dim salida As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text += " "
+        s1()
+    End Sub
+    Sub s1()
+        If TextBox1.Text(contador) = "1" Then
+            contador += 1
+            salida += "1"
+            Label1.Text = salida
+            MsgBox("Encuentra 1, lo deja igual, derecha")
+            s2()
+        ElseIf TextBox1.Text(contador) <> "1" Then
+            MsgBox("NO SE ACEPTA")
+        End If
+    End Sub
+    Sub s2()
+        If TextBox1.Text(contador) = "1" Then
+            contador += 1
+            salida += "1"
+            Label1.Text = salida
+            MsgBox("Encuentra 1, lo deja igual, derecha")
+            s2()
+        ElseIf TextBox1.Text(contador) = "0" Then
+            contador += 1
+            salida += "0"
+            Label1.Text = salida
+            MsgBox("Encuentra 0, lo deja igual, derecha")
+            s1()
+        ElseIf TextBox1.Text(contador) = " " Then
+            MsgBox("se acepta la cadena")
+
+        End If
+
+    End Sub
+End Class
