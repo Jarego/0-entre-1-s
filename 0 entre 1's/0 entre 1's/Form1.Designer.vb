f21d2cdcab1aec1dba54789faf12da8dc411373a
Jarego <jaregocreepy@hotmail.com>
2019-04-28 14:13:33 -0500

    Add files via upload

--- 0 entre 1's/0 entre 1's/Form1.Designer.vb
+++ 0 entre 1's/0 entre 1's/Form1.Designer.vb
@@ -22,46 +22,71 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(168, 57)
+        Me.TextBox1.Location = New System.Drawing.Point(107, 15)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 20)
         Me.TextBox1.TabIndex = 0
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(41, 68)
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Evaluar"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(168, 100)
+        Me.Label1.Location = New System.Drawing.Point(104, 38)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Label1"
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "basico.png")
+        Me.ImageList1.Images.SetKeyName(1, "q0.png")
+        Me.ImageList1.Images.SetKeyName(2, "q1.png")
+        Me.ImageList1.Images.SetKeyName(3, "q2.png")
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global._0_entre_1_s.My.Resources.Resources.basico1
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 63)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(256, 81)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(290, 178)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -70,4 +95,6 @@ Partial Class Form1
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ImageList1 As ImageList
 End Class
--- 0 entre 1's/0 entre 1's/Form1.vb
+++ 0 entre 1's/0 entre 1's/Form1.vb
@@ -3,6 +3,8 @@
     Dim salida As String
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         TextBox1.Text += " "
+        PictureBox1.Image = ImageList1.Images(1)
+        MsgBox("inicio")
         s1()
     End Sub
     Sub s1()
@@ -10,6 +12,7 @@
             contador += 1
             salida += "1"
             Label1.Text = salida
+            PictureBox1.Image = ImageList1.Images(2)
             MsgBox("Encuentra 1, lo deja igual, derecha")
             s2()
         ElseIf TextBox1.Text(contador) <> "1" Then
@@ -21,15 +24,18 @@
             contador += 1
             salida += "1"
             Label1.Text = salida
+            PictureBox1.Image = ImageList1.Images(1)
             MsgBox("Encuentra 1, lo deja igual, derecha")
             s2()
         ElseIf TextBox1.Text(contador) = "0" Then
             contador += 1
             salida += "0"
             Label1.Text = salida
+            PictureBox1.Image = ImageList1.Images(1)
             MsgBox("Encuentra 0, lo deja igual, derecha")
             s1()
         ElseIf TextBox1.Text(contador) = " " Then
+            PictureBox1.Image = ImageList1.Images(3)
             MsgBox("se acepta la cadena")
 
         End If
